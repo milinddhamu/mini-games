ffa425884f19427ffc8bdc0e03ebd526ae0e614e
Milind Dhamu <milind.dhamu.111@gmail.com>
2023-12-24 14:15:46 +0530

    Update: TypingTest added to library

---
+++ src/app/typetest/page.js
@@ -0,0 +1,16 @@
+"use client"
+import TestBase from "@/TypeTest/TestBase";
+
+const page = () => {
+  const sentence = "Nuclear power is the use of nuclear reactions to produce electricity. Nuclear power can be obtained from nuclear fission, nuclear decay and nuclear fusion reactions. Presently, the vast majority of electricity from nuclear power is produced by nuclear fission of uranium and plutonium in nuclear power plants.";
+  
+  return (
+    <>
+    
+    <TestBase sentence={sentence}/>
+
+    </>
+  );
+}
+
+export default page;
