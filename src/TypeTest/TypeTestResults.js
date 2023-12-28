312a64f383212897bd0ef06ab7916021b70c6c4b
Milind Dhamu <milind.dhamu.111@gmail.com>
2023-12-28 11:51:38 +0530

    New: Restart TypeTest after result

--- src/TypeTest/TypeTestResults.js
+++ src/TypeTest/TypeTestResults.js
@@ -1,14 +1,14 @@
 "use client"
 import { TiRefresh } from "react-icons/ti";
-import {Tooltip,Divider} from "@nextui-org/react";
+import {Tooltip,Chip} from "@nextui-org/react";
 import {useRef,useEffect} from "react";
 import { animate } from "framer-motion";
 export default function TypeTestResults({data,handleRestart}){
   const resultMetrics = [
-    { name: "Correct Words", data: data?.correctWords,color:"success",textColor:"green"},
-    { name: "Incorrect Words", data: data?.incorrectWords,color:"danger",textColor:"red" },
-    { name: "Skipped Words", data: data?.skippedWords,color:"",textColor:"slate" },
-    { name: "Extra Words", data: data?.extraWords,color:"warning",textColor:"yellow" },
+    { name: "Correct Words", data: data?.correctWords || 0,color:"success",textColor:"green"},
+    { name: "Incorrect Words", data: data?.incorrectWords || 0,color:"danger",textColor:"red" },
+    { name: "Skipped Words", data: data?.skippedWords || 0,color:"",textColor:"slate" },
+    { name: "Extra Words", data: data?.extraWords || 0,color:"warning",textColor:"yellow" },
   ];
   function handleReplace(){
     window.location.reload()
@@ -40,11 +40,11 @@ export default function TypeTestResults({data,handleRestart}){
       <h1 className="text-3xl font-bold uppercase text-start tracking-tightest opacity-80">Results</h1>
       <div className="flex flex-col justify-center items-center w-full max-w-fit p-4 px-8 gap-4">
         <div className="flex flex-row w-full justify-center items-end gap-1">
-          <Counter from={0} to={data.wordsPerMinute}/>
+          <Counter from={0} to={data?.wordsPerMinute || 0}/>
           <h1 className="text-3xl font-bold tracking-tightest">WPM</h1>
 
         </div>
-        <h2 className="text-lg tracking-widest uppercase opacity-70">at {data.accuracy}{"%"} accuracy</h2>
+        <h2 className="text-lg tracking-widest uppercase opacity-70">at {data?.accuracy || 0}{"%"} accuracy</h2>
         <div className="flex flex-row gap-4 items-center w-full">{resultMetrics.map((result , index)=>(
           <Tooltip 
           showArrow={true}
@@ -54,7 +54,7 @@ export default function TypeTestResults({data,handleRestart}){
           color={result.color}
           >
             <div className="flex flex-row items-baseline gap-4">
-              <h1 className={`text-5xl font-bold uppercasetext opacity-70 hover:opacity-100 hover:cursor-pointer transition-all ease-out`}>{result.data}</h1>
+              <h1 className={`text-5xl font-bold uppercasetext opacity-70 hover:opacity-100 hover:cursor-pointer transition-all ease-out`}>{result?.data}</h1>
               {result.name !== "Extra Words" &&
               <span className="flex h-8 border-1 border-gray-500/50 opacity-50"></span> }
             </div>
@@ -65,14 +65,16 @@ export default function TypeTestResults({data,handleRestart}){
 
 
       </div>
+      <div className="flex flex-col items-center">
       <Tooltip 
       placement="bottom"
-      content={
-        <h1 className="p-2">Start over</h1>
-      }>
-      <button onClick={handleRestart} className="px-20 py-14 m-8 border-1 border-gray-500/50 group"><TiRefresh  className="text-5xl opacity-40 group-focus:opacity-60 group-focus:rotate-45 transition-all ease-in duration-150"/>
+      content="Start over">
+      <button onClick={handleRestart} className="px-10 py-8 m-8 border-1 border-gray-500/50 group"><TiRefresh  className="text-5xl opacity-40 group-focus:opacity-60 group-focus:rotate-45 transition-all ease-in duration-150"/>
       </button>
       </Tooltip>
+      <Chip size="sm">Press tab + enter to restart</Chip>
+
+    </div>
     </div>
     </>
   )
